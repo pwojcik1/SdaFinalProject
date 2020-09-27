430816d990833f208759fe8c62c4fc6f0fbe5d69
Jarosław Pitura <jarekpitura@gmail.com>
2020-09-27 10:59:36 +0200

    products

--- src/main/java/domain/Product.java
+++ src/main/java/domain/Product.java
@@ -1,4 +1,20 @@
 package domain;
 
+import lombok.*;
+
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
 public class Product {
+
+    private Integer id;
+    private String name;
+    private Integer quantity;
+
+    List<Integer>RecipeId;
+
+
 }
--- src/main/java/external/ProductEntity.java
+++ src/main/java/external/ProductEntity.java
@@ -1,4 +1,32 @@
 package external;
 
-public class ProductEntity {
+import domain.Recipe;
+import domain.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    @Entity
+    @Table(name = "products")
+    public class ProductEntity {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+        private Integer id;
+        private String name;
+
+        @OneToMany(mappedBy = "user")
+        private Set<UserEntity> users;
+        @OneToMany (mappedBy = "user")
+        private Set<Recipe>recipes;
+
 }
