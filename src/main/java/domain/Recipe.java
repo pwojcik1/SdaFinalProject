09adb7017437185342b4591beb76486fc1536749
pwojcik1 <kyrtap44@gmail.com>
2020-09-27 10:59:50 +0200

    feature/recipe

--- src/main/java/domain/Recipe.java
+++ src/main/java/domain/Recipe.java
@@ -1,4 +1,18 @@
 package domain;
 
+import lombok.*;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
 public class Recipe {
+    private int id;
+    private String name;
+    private String description;
+    private List<Integer> productId;
+
 }
--- src/main/java/external/RecipeEntity.java
+++ src/main/java/external/RecipeEntity.java
@@ -1,4 +1,25 @@
 package external;
 
+import lombok.*;
+
+
+import javax.persistence.*;
+import java.util.Set;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Entity
+@Table(name = "recipes")
 public class RecipeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String description;
+    @OneToMany(mappedBy = "recipe")
+    private Set<ProductEntity> products;
+
 }
