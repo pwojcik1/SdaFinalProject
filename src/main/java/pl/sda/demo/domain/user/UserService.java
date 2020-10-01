9fa6ed86e312a3fd1476d84a1f9bf746c2616658
pwojcik1 <kyrtap44@gmail.com>
2020-10-01 17:37:03 +0200

    UserService

--- src/main/java/pl/sda/demo/domain/user/UserService.java
+++ src/main/java/pl/sda/demo/domain/user/UserService.java
@@ -10,21 +10,55 @@ public class UserService {
 
     private final UserRepository userRepository;
 
-    public void addProductToFridge(Product product) {
-        userRepository.addProductToFridge(product);
+    void createUser(User user) {
+        userRepository.findByUsername(user.getUsername())
+                .ifPresent(u -> {
+                    throw new IllegalStateException("Username already taken");
+                });
+        userRepository.createUser(user);
     }
 
-    public void removeProductFromFridge(Integer id) {
-        if (!userRepository.isProductInFridge(id)) {
-            userRepository.removeProductFromFridge(id);
-        }
-        throw new IllegalArgumentException("You dont have this product in fridge");
+    void updateUser(User user) {
+        userRepository.findByUsername(user.getUsername())
+                .filter(u -> !u.getId().equals(user.getId()))
+                .ifPresent(user1 -> {
+                    throw new IllegalStateException("Cannot update user with different id");
+                });
+        userRepository.updateUser(user);
     }
 
-    public void addRecipeToFavourites(Integer id) {
-        if (userRepository.isAlreadyFavourite(id)) {
-            throw new IllegalStateException("Already favourite");
-        }
-        userRepository.addRecipeToFavourites(id);
+    void deleteUser(int id) {
+        userRepository.deleteUser(id);
     }
+
+    void addProductToFridge(Product product, User user) {
+        if (user.getProductId().contains(product.getId())) {
+            throw new IllegalStateException("Product already in fridge");
+        }
+        userRepository.addProductToFridge(product, user);
+    }
+
+    void removeProductFromFridge(int id, User user) {
+        if (!user.getProductId().contains(id)) {
+            throw new IllegalStateException("You dont have this product in your fridge");
+        }
+        userRepository.removeProductFromFridge(id, user);
+    }
+
+    void updateProductInFridge(Product product, User user) {
+        userRepository.getProductFromFridgeByName(product.getName(), user)
+                .filter(p -> !p.getId().equals(product.getId()))
+                .ifPresent(product1 -> {
+                    throw new IllegalStateException("Cannot update product with different id");
+                });
+        userRepository.updateProductInFridge(product, user);
+    }
+
+    void addRecipeToFavourites(int id, User user) {
+        if (user.getRecipeId().contains(id)) {
+            throw new IllegalStateException("Recipe already in favourites");
+        }
+        userRepository.addRecipeToFavourites(id, user);
+    }
+
 }
