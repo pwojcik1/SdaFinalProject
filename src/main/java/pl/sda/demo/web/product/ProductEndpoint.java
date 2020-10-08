565ab772d75b624d205648339e9930c4a98bf853
pwojcik1 <kyrtap44@gmail.com>
2020-10-08 17:43:10 +0200

    small problems with merging :)

--- src/main/java/pl/sda/demo/web/product/ProductEndpoint.java
+++ src/main/java/pl/sda/demo/web/product/ProductEndpoint.java
@@ -1,40 +1,6 @@
 package pl.sda.demo.web.product;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
-import pl.sda.demo.domain.product.Product;
-import pl.sda.demo.domain.product.ProductService;
-import pl.sda.demo.external.product.DatabaseProductRepository;
-
-import java.util.Optional;
-
-
-@RestController
-@RequestMapping("/api/product")
-@RequiredArgsConstructor
 public class ProductEndpoint {
-    private final ProductService productService;
-    private final DatabaseProductRepository databaseProductRepository;
-
-    @PostMapping
-    void addProductToLibrary(@RequestBody Product product) {
-        productService.addProductToLibrary(product);
-    }
-
-    @PutMapping
-    void updateProductInLibrary(@RequestBody Product product) {
-        productService.updateProductInLibrary(product);
-    }
-
-    @DeleteMapping
-    void deleteProductFromLibrary(@RequestParam int id) {
-        productService.deleteProductFromLibrary(id);
-    }
-
-    @GetMapping(("/{name}"))
-    Optional<Product> getProductByName(@RequestParam String name){
-        return databaseProductRepository.getProductByName(name);
-    }
 }
 
 
--- src/main/java/pl/sda/demo/web/recipe/RecipeEndpoint.java
+++ src/main/java/pl/sda/demo/web/recipe/RecipeEndpoint.java
@@ -1,43 +1,4 @@
 package pl.sda.demo.web.recipe;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
-import pl.sda.demo.domain.product.Product;
-import pl.sda.demo.domain.recipe.Recipe;
-import pl.sda.demo.domain.recipe.RecipeService;
-
-import java.util.List;
-import java.util.Set;
-
-@RestController
-@RequestMapping("/api/recipe")
-@RequiredArgsConstructor
 public class RecipeEndpoint {
-
-    private final RecipeService recipeService;
-
-    @PostMapping
-    void addRecipeToDb(@RequestBody Recipe recipe) {
-        recipeService.addRecipeToDb(recipe);
-    }
-
-    @PutMapping
-    void updateRecipeInDb(@RequestBody Recipe recipe) {
-        recipeService.updateRecipeInDb(recipe);
-    }
-
-    @DeleteMapping
-    void deleteRecipeFromDb(@RequestParam int id) {
-        recipeService.deleteRecipeFromDb(id);
-    }
-
-    @GetMapping("/{name}")
-    Recipe findByRecipeName(@RequestParam String name) {
-        return recipeService.findByRecipeName(name);
-    }
-
-    @GetMapping("{products}")
-    Set<Recipe> findByProducts(@RequestParam List<Product> products) {
-        return recipeService.findByProducts(products);
-    }
 }
